d3f9385ec6d3463fe49a9acc3cb42c70eba2bf36
zhourongcode <zhourongcode@163.com>
2018-01-02 21:43:57 +0800

    es5 module as

--- module.js
+++ module.js
@@ -12,3 +12,20 @@ var year = 1958
 export {firstName, lastName, year}
 
 //  如上两种方式效果一样， 推荐后者
+
+export function mutiply(x, y) {
+  return x * y
+}
+
+// 导出一个函数 也可到处一个类(class)
+
+function v1() {...}
+function v2() {...}
+
+export {
+  v1 as streamV1,
+  v2 as streamV2,
+  v2 as stramLatestVersion
+}
+
+//  as 重命名 v1 & v2
