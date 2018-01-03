0a4a3a835336fef30665fd3b82e6ffc96bced4b1
changxianglin <281532612@qq.com>
2018-01-03 09:37:06 +0800

    import

--- module.js
+++ module.js
@@ -52,3 +52,11 @@ setTimeout(() => foo = 'baz', 500)
 // es6 箭头函数  输出变量 foo ， 值为 bar ， 500 秒后变成 baz.
 
 //  exprot 全部完结。
+
+import 命令加载模块
+
+import{firstName, lastName, year} from './profile'
+
+function setName(element) {
+  element.textContent = firstName + '' + lastName
+}
