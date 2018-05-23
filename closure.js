caf712e416cb3df7eec0fe416c190bb94bbaa405
changxianglin <281532612@qq.com>
2018-05-23 23:29:01 +0800

    function parameter

--- closure.js
+++ closure.js
@@ -9,3 +9,34 @@ var foo = function(val) {
 }
 
 // 以上是一个典型的闭包，简单的说就是带有数据的函数
+
+ // demo1：
+function createFunction() {
+  var result = new Array()
+
+  for(var i = 0; i < 10; i++) {
+    result[i] = function() {
+      return i
+    }
+  }
+
+  return result
+}
+
+
+// demo2
+function createFunction() {
+  var result = new Array()
+
+  for(var i = 0; i < 10; i++) {
+    result[i] = function(num) {
+      return function() {
+        return num
+      }
+    }(i)
+  }
+
+  return result
+}
+
+// 函数参数按值传递
