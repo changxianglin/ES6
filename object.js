96a6b835a59e8ecc99c4086c89e62eb583286f31
changxianglin <281532612@qq.com>
2019-12-14 23:55:09 +0800

    object extension

--- object.js
+++ object.js
@@ -13,3 +13,50 @@ function f(x, y) {
 function f(x, y) {
   return {x: x, y: y}
 }
+
+var o = {
+  method() {
+    return 'Hello!'
+  }
+}
+
+var o = {
+  method: function() {
+    return 'Hello!'
+  }
+}
+
+var Person = {
+  name: '张三',
+  brith,
+  hello() {console.log('我的名字是,', this.name)}
+}
+
+function getPoint() {
+  var x = 1
+  var y = 10
+  return { x, y }
+}
+getPoint() // {x:1, y:10}
+
+
+var ms = {}
+
+funtion getItem(key) {
+  return key in ms ? ms[key]: null
+}
+
+function setItem(key, value) {
+  ms[key] = value
+}
+
+function clear() {
+  ms = {}
+}
+
+module.exports = { getItem, setItem, clear } //等同于
+module.exports = {
+  getItem: getItem,
+  setItem: setItem,
+  clear: clear,
+}
